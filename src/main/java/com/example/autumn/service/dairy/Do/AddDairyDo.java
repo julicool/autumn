3fdfaef8846f19a909262a1d77a55f125188e781
zhaojiaqi <zhaojiaqi@youzan.com>
2021-02-08 11:28:35 +0800

    修改数据库配置信息

--- src/main/java/com/example/autumn/service/dairy/Do/AddDairyDo.java
+++ src/main/java/com/example/autumn/service/dairy/Do/AddDairyDo.java
@@ -8,11 +8,23 @@ import lombok.Data;
  */
 @Data
 public class AddDairyDo {
+
+    // 文章标题
     private String title;
+
+    // 文章内容
     private String content;
+
+    // 文章是否显示
     private Integer isShow;
+
+    // 写作地点
     private String location;
+
+    // 当时天气
     private String weather;
+
+    // 标签
     private String tag1;
     private String tag2;
     private String tag3;
